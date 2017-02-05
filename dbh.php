316868c2c8c91da1759cbd1b721a8da468d7bb40
ahtunis <ahtunis62@gmail.com>
2017-02-05 17:10:09 -0500

    added in variables for declaring connection strings

--- dbh.php
+++ dbh.php
@@ -3,14 +3,14 @@
 
 	/////// Update your database login details here /////
 	$dbhost_name = "localhost"; // Your host name 
-	$database = "soccerproject";       // Your database name
+	$database = "expenses";       // Your database name
 	$username = "root";            // Your login userid 
 	$password = "";            // Your password 
 	//////// End of database details of your server //////
 
 	//////// Do not Edit below /////////
 	try {
-	$con = mysqli_connect($dbhost_name, $username, $password, $database);
+	$conn = mysqli_connect($dbhost_name, $username, $password, $database);
 	//db conn string
 
 		if(!$conn){

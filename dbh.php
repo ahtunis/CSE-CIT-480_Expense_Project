d562d388c54066bcc5fa46ad902edff5272cf3f7
ahtunis <ahtunis62@gmail.com>
2017-02-05 17:03:59 -0500

    set up local db

--- dbh.php
+++ dbh.php
@@ -1,7 +1,25 @@
-<?php
-//db conn string
-$conn=mysqli_connect("localhost","root","","login");
+	<?php
 
-if(!$conn){
-	die("Connection failed: ".mysqli_connect_error());
-}
+
+	/////// Update your database login details here /////
+	$dbhost_name = "localhost"; // Your host name 
+	$database = "soccerproject";       // Your database name
+	$username = "root";            // Your login userid 
+	$password = "";            // Your password 
+	//////// End of database details of your server //////
+
+	//////// Do not Edit below /////////
+	try {
+	$con = mysqli_connect($dbhost_name, $username, $password, $database);
+	//db conn string
+
+		if(!$conn){
+			die("Connection failed: ".mysqli_connect_error());
+		}
+
+	} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+	}
+	
+?> 

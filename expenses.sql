47146ede92ec9c76738b6f94063e99fac9180fec
ahtunis <ahtunis62@gmail.com>
2017-02-05 17:57:46 -0500

    Added in business table and references to userid

--- expenses.sql
+++ expenses.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2017 at 10:36 PM
+-- Generation Time: Feb 05, 2017 at 11:57 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `expenses`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business`
+--
+
+CREATE TABLE IF NOT EXISTS `business` (
+  `BusinessId` int(11) NOT NULL AUTO_INCREMENT,
+  `BusinessName` varchar(255) NOT NULL,
+  `AddressLineOne` varchar(255) NOT NULL,
+  `AddressLineTwo` varchar(255) DEFAULT NULL,
+  `City` varchar(255) NOT NULL,
+  `State` varchar(255) NOT NULL,
+  `Zip` varchar(15) NOT NULL,
+  PRIMARY KEY (`BusinessId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `business`
+--
+
+INSERT INTO `business` (`BusinessId`, `BusinessName`, `AddressLineOne`, `AddressLineTwo`, `City`, `State`, `Zip`) VALUES
+(1, 'Test Business', '1234 Main', NULL, 'Auburn Hills', 'Mi', '48326');
 
 -- --------------------------------------------------------
 
@@ -56,7 +80,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `businessId` int(11) NOT NULL,
   `userIsAdmin` bit(1) NOT NULL,
   PRIMARY KEY (`userId`),
-  KEY `userId` (`userId`)
+  KEY `userId` (`userId`),
+  KEY `businessId` (`businessId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
@@ -64,12 +89,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`userId`, `email`, `password`, `firstName`, `middleName`, `lastName`, `businessId`, `userIsAdmin`) VALUES
-(1, 'email@test.com', '12345', '', NULL, '', 0, b'0'),
-(2, 'alex@test.com', '123456', '', NULL, '', 0, b'0'),
-(3, 'alex1@test.com', '123456', '', NULL, '', 0, b'0'),
-(4, 'alex@alex.com', '123456', '', NULL, '', 0, b'0'),
-(5, 'alex@alex.com', '123456', '', NULL, '', 0, b'0'),
-(6, '1@1.com', '123456', '', NULL, '', 0, b'0');
+(1, 'email@test.com', '12345', '', NULL, '', 1, b'0'),
+(2, 'alex@test.com', '123456', '', NULL, '', 1, b'0'),
+(3, 'alex1@test.com', '123456', '', NULL, '', 1, b'0'),
+(4, 'alex@alex.com', '123456', '', NULL, '', 1, b'0'),
+(5, 'alex@alex.com', '123456', '', NULL, '', 1, b'0'),
+(6, '1@1.com', '123456', '', NULL, '', 1, b'0');
 
 -- --------------------------------------------------------
 
@@ -99,6 +124,12 @@ INSERT INTO `user_roles` (`userId`, `roleId`) VALUES
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`businessId`) REFERENCES `business` (`BusinessId`);
 
 --
 -- Constraints for table `user_roles`
